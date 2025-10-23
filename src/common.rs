92dae37f1d614d55ae671fce7f805f49facf81fb
acgkiss <anime@acgkiss.com>
2025-10-23 16:42:12 +0800

    更新子模块

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://101.237.129.79".to_owned()
+    "http://101.237.129.79:21113".to_owned()
 }
 
 #[inline]
